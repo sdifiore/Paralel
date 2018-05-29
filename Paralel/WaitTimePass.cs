147f723435e15c21ae445dea44141f2142b02fdd
Sergio Di Fiore <difiores@outlook.com>
2018-05-29 13:59:03 -0300

    Wait Time to Pass

--- Paralel/WaitTimePass.cs
+++ Paralel/WaitTimePass.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Paralel
 {
-    class Program
+    class WaitTimePass
     {
         static void Main(string[] args)
         {
